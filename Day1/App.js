0ef724cce98521ebfad42c401249b5c14cb91949
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-10-31 11:04:16 +1300

    Create App.js

---
+++ Day1/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from './components/Weather';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
