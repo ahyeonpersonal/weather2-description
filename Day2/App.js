c312b817c6e7eb6ce0034242833abffe852d8742
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-10-31 11:23:22 +1300

    Create App.js

---
+++ Day2/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from './components/Weather';
+
+const API_KEY = "18fff390d86582a57fabf47a890d2816"; //1. put my API key
+
+class App extends Component {
+  
+  // 2. Calling api
+  getWeather = async () => {
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid${API_KEY}=&units=metric`);
+    //`` : let you to inject the variable that you defiend within your files
+
+    //3.Convert to json file : transfer API to readable files
+    const data = await api_call.json();
+    
+  }
+
+  render() {
+    return (
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
+    );
+  }
+}
+
+export default App;
