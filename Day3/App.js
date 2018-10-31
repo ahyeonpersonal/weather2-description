d8dbb7c10b637d1ddfeba76f76e5fe961ccf030b
Ahyeon Na_2 <44590651+ahyeonpersonal@users.noreply.github.com>
2018-10-31 14:19:00 +1300

    Update App.js

--- Day3/App.js
+++ Day3/App.js
@@ -9,8 +9,8 @@ const API_KEY = "18fff390d86582a57fabf47a890d2816"; //1. put my API key
 class App extends Component {
   
   // 2. Calling api
-  getWeather = async () => {
-
+  getWeather = async (e) => {
+    e.preventDefault(); // 7. prevent page refreshing when submit the button
     const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid${API_KEY}=&units=metric`);
     //`` : let you to inject the variable that you defiend within your files
 
